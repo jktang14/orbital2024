fb4dd58cd947f16b19751631287cedc1d90dd003
jktang14 <tangboy14@gmail.com>
2024-05-23 22:35:23 +0800

    Add pieces to grid

--- app/reversi/reversi.jsx
+++ app/reversi/reversi.jsx
@@ -4,10 +4,21 @@ import '../ui/style.css'
 import './game-logic'
 
 const Reversi = () => {
+    {/*Create grid*/}
     const CreateGrid = () => {
         let items = [];
         for (let i = 0; i < 64; i++) {
-            items.push(createElement('div', {id: 'grid-item', className: `grid-item-${i}`}));
+            if (i == 27 || i == 36) {
+                let piece = createElement('img', {src:'black.png'});
+                let block = createElement('div', {className: 'piece'}, piece); 
+                items.push(createElement('div', {id: 'grid-item', className: `grid-item-${i}`}, block));
+            } else if (i == 28 || i == 35) {
+                let piece = createElement('img', {src:'white.png'});
+                let block = createElement('div', {className: 'piece'}, piece); 
+                items.push(createElement('div', {id: 'grid-item', className: `grid-item-${i}`}, block));
+            } else {
+                items.push(createElement('div', {id: 'grid-item', className: `grid-item-${i}`}));
+            }
         }
         return items;
     }
