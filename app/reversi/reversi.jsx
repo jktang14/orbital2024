79a12c71ece1c0b72902e3744bc1de1824a28f3e
jktang14 <tangboy14@gmail.com>
2024-05-25 00:04:43 +0800

    Add logic for when user selects a move

--- app/reversi/reversi.jsx
+++ app/reversi/reversi.jsx
@@ -7,12 +7,21 @@ const Reversi = () => {
     const [match, setMatch] = useState(new game());
     const [board, setBoard] = useState(match.board);
     const [currentPlayer, setCurrentPlayer] = useState(match.currentPlayer);
+
+    function handleCellClick(rowIndex, colIndex) {
+        if (match.isValidMove(rowIndex, colIndex)) {
+            match.makeMove(rowIndex, colIndex);
+            setBoard(match.board);
+            setCurrentPlayer(match.getOpponent());
+        }
+    }
+
     return (
         <div className="container">
             {match.board.map((row, rowIndex) => (
                 <div className="row" key={rowIndex}>
                 {row.map((cell, colIndex) => (
-                    <div className="cell" key={colIndex} onClick={handleCellClick(rowIndex, colIndex)}>
+                    <div className="cell" key={colIndex} onClick={() => handleCellClick(rowIndex, colIndex)}>
                         {cell == 'black' && <img src="black.png" alt="Black piece" />}
                         {cell == 'white' && <img src="white.png" alt="White piece" />}
                     </div>
