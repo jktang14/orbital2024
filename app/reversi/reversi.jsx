83f7fbbf7e5af6baac4de85a06f64dacb68c3342
jktang14 <tangboy14@gmail.com>
2024-05-25 20:46:52 +0800

    Implement game ending logic

--- app/reversi/reversi.jsx
+++ app/reversi/reversi.jsx
@@ -7,28 +7,51 @@ const Reversi = () => {
     const [match, setMatch] = useState(new game());
     const [board, setBoard] = useState(match.board);
     const [currentPlayer, setCurrentPlayer] = useState(match.currentPlayer);
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        checkStatus();
+    }, [board]);
+
+    function checkStatus() {
+        const result = match.checkGameStatus();
+        if (result.status === 'win') {
+            setMessage(`${result.winner} wins!`);
+        } else if (result.status === 'draw') {
+            setMessage('The game is a draw!');
+        } else if (result.status === 'skip') {
+            setMessage(result.message);
+            setCurrentPlayer(match.getOpponent()); // swap back to original player
+        } else {
+            setMessage("");
+        }
+    }
 
     function handleCellClick(rowIndex, colIndex) {
         if (match.isValidMove(rowIndex, colIndex)) {
             match.makeMove(rowIndex, colIndex);
             setBoard(match.board);
+            setCurrentPlayer(match.currentPlayer); // current player has internally swapped within makeMove
+            checkStatus();
         }
     }
 
     return (
-        <div className="container">
-            {match.board.map((row, rowIndex) => (
-                <div className="row" key={rowIndex}>
-                {row.map((cell, colIndex) => (
-                    <div className="cell" key={colIndex} onClick={() => handleCellClick(rowIndex, colIndex)}>
-                        {cell == 'black' && <img src="black.png" alt="Black piece" />}
-                        {cell == 'white' && <img src="white.png" alt="White piece" />}
-                        {match.isValidMove(rowIndex, colIndex) && <div className='valid-move-indicator'></div>}
+        <>
+            <div className="container">
+                {match.board.map((row, rowIndex) => (
+                    <div className="row" key={rowIndex}>
+                    {row.map((cell, colIndex) => (
+                        <div className="cell" key={colIndex} onClick={() => handleCellClick(rowIndex, colIndex)}>
+                            {cell == 'black' && <img src="black.png" alt="Black piece" />}
+                            {cell == 'white' && <img src="white.png" alt="White piece" />}
+                            {match.isValidMove(rowIndex, colIndex) && <div className='valid-move-indicator'></div>}
+                        </div>
+                    ))}
                     </div>
                 ))}
-                </div>
-            ))}
-        </div>
+            </div>
+        </>
     );
 }
 
