1bbf24d6cfcd0ec7c76914bec1b533ad20cb0b6b
jktang14 <tangboy14@gmail.com>
2024-05-28 01:14:06 +0800

    Add toolbar to page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Reversi from './reversi/reversi.jsx';
+import ResponsiveAppBar from './navbar.jsx';
 
 function Home() {
   return (
     <div>
+      <ResponsiveAppBar/>
       <Reversi/>
     </div>
   );
