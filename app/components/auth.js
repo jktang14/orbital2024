d13b0203cdd74b7f5194f33b937abb9178c5acd1
jktang14 <tangboy14@gmail.com>
2024-05-28 17:12:29 +0800

    Add auth.js

---
+++ app/components/auth.js
@@ -0,0 +1,3 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { auth, db } from './firebase';
