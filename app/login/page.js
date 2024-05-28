d3918eaa65b085f2b45ad1d09482b02e5314e092
jktang14 <tangboy14@gmail.com>
2024-05-28 23:52:27 +0800

    Move login.js code to page.js

---
+++ app/login/page.js
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react';
+import { useState } from 'react';
+import { LoginUser } from '../components/auth';
+import { useRouter } from 'next/navigation';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+        let {user, username} = await LoginUser(email, password);
+        // Store username
+        localStorage.setItem('username', username);
+        router.push('../game') // Navigate to game page
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <label>Email: </label>
+                <input type="text" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/><br/>
+                <label>Password: </label>
+                <input type="text" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/><br/>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
