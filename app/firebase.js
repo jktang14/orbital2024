a00d00eba920f7555be0c859f3ab11a4174623a1
jktang14 <tangboy14@gmail.com>
2024-05-28 16:46:12 +0800

    Initialise authentication and firestore

--- app/firebase.js
+++ app/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyA0KFSLHWL_2Oiv2PW_J-2Rp1GVf-FwtgA",
   authDomain: "reversiplus-52db7.firebaseapp.com",
@@ -13,3 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {auth, db};
