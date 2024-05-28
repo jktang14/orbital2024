2b4ea0805894a72c1c2ff6aba9d159341d2b1ce5
jktang14 <tangboy14@gmail.com>
2024-05-28 23:54:16 +0800

    Update paths to reflect new directories

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Registration from './registration';
+import Registration from './registration/page';
+import Login from './login/page';
 
 function Page() {
   return (
     <div>
       <Registration/>
+      <Login/>
     </div>
   );
 }
