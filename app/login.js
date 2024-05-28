a9299576bc7c2cf06e2a46c4eb7e99787ea49ee7
jktang14 <tangboy14@gmail.com>
2024-05-28 23:46:14 +0800

    Add inputs for users to login

--- app/login.js
+++ app/login.js
@@ -1,5 +1,6 @@
 'use client';
 import React from 'react';
+import { useState } from 'react';
 import { LoginUser } from './components/auth';
 import { useRouter } from 'next/navigation';
 
@@ -9,18 +10,24 @@ function Login() {
     const router = useRouter();
 
     async function handleLogin(e) {
-        e.preventdefault();
+        e.preventDefault();
         let {user, username} = await LoginUser(email, password);
         // Store username
         localStorage.setItem('username', username);
-        router.push('./game') // Push to login page
+        router.push('/game') // Navigate to game page
     }
-  
-  
-  
+
     return (
-    <div>Login</div>
-  )
+        <div>
+            <form onSubmit={handleLogin}>
+                <label>Email: </label>
+                <input type="text" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/><br/>
+                <label>Password: </label>
+                <input type="text" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/><br/>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
