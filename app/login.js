d4fc7a742315ceecd68f5e5a7ee570b610e66b57
jktang14 <tangboy14@gmail.com>
2024-05-28 21:21:49 +0800

    Add login.js

---
+++ app/login.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import { LoginUser } from './components/auth';
+import { Router, useRouter } from 'next/navigation';
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login;
