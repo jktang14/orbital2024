be8814e3f3e9a92271b30d6687646923dd949063
jktang14 <tangboy14@gmail.com>
2024-05-28 21:17:03 +0800

    Add logic for registration page

--- app/registration.js
+++ app/registration.js
@@ -1,27 +1,35 @@
+'use client';
 import { useState } from 'react';
 import React from 'react';
-import { signUpUser } from './components/auth';
-import { Router } from 'next/router';
+import { SignUpUser } from './components/auth';
+import { Router, useRouter } from 'next/navigation';
 
-function registration() {
+function Registration() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
+    const router = useRouter();
+    
     async function handleRegistration(e) {
         // Prevent page refresh
         e.preventDefault();
-        try {
-            await signUpUser(email, password, username);
-            Router.push('');
-        } catch (err) {
-
-        }
+        await SignUpUser(email, password, username);
+        router.push('');
     }
-  
-  
+    
     return (
-    <div>registration</div>
-  )
+        <>
+            <form onSubmit={handleRegistration}>
+                <label>Email: </label>
+                <input type="text" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/><br/>
+                <label>Password: </label>
+                <input type="text" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/><br/>
+                <label>Username: </label>
+                <input type="text" placeholder='Username' value={username} onChange={e => setUsername(e.target.value)}/><br/>
+                <button type="submit">Register</button>
+            </form>
+        </>
+    );
 }
 
-export default registration;
+export default Registration;
