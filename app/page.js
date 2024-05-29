e72dc7637c245866c09c9a74b4513b8fdf657d67
jktang14 <tangboy14@gmail.com>
2024-05-29 20:32:29 +0800

    Move registration and login page to separate pages

--- app/page.js
+++ app/page.js
@@ -1,14 +1,26 @@
+'use client';
 import React from 'react';
 import Registration from './registration/page';
 import Login from './login/page';
+import { useRouter } from 'next/navigation';
 
 function Page() {
-  return (
-    <div>
-      <Registration/>
-      <Login/>
-    </div>
-  );
+	const router = useRouter();
+
+	function handleRegisterClick(e) {
+		router.push('/registration');
+	}
+
+	function handleLoginClick(e) {
+		router.push('/login');
+	}
+
+	return (
+		<div>
+			<button onClick={handleRegisterClick}>Register</button>
+			<button onClick={handleLoginClick}>Login</button>
+    	</div>
+  	);
 }
 
 export default Page;
