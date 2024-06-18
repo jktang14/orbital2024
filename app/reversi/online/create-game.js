ecddccf7d5b47c4d2c2ed1765fb9303e872790e0
jktang14 <tangboy14@gmail.com>
2024-06-18 16:41:31 +0800

    Change implementation of create game

--- app/reversi/online/create-game.js
+++ app/reversi/online/create-game.js
@@ -1,18 +1,37 @@
 import { realtimeDatabase } from "@/app/firebase";
-import { ref, push, set } from "firebase/database";
 import game from "../game-logic";
+import { ref, push, set } from "firebase/database";
 
-export const createGame = async (playerId) => {
-    // Get game id
-    const gameId = push(ref(realtimeDatabase, 'games')).key;
-    const gameRef = ref(realtimeDatabase, `games/${gameId}`);
+const createNewGame = (boardSize, username, setGameId, setMatch, setBoard, setBoardSize, setCurrentPlayer, setMessage, setIsGameActive, setHasGameStarted, timer, setBlackTime, setWhiteTime) => {
+    const newGameRef = ref(realtimeDatabase, 'games');
+    const newGameKey = push(newGameRef).key;
 
-    // Create new instance of a game
-    const gameInstance = new game();
+    setGameId(newGameKey);
+    
+    const newMatch = new game(boardSize);
 
-    await set(gameRef, {
-        board : gameInstance.board,
-        players: {playerId: "Black"},
-        moves : {}
-    })
-}
+    set(ref(realtimeDatabase, `games/${newGameKey}`), {
+        match: newMatch,
+        board: newMatch.board,
+        boardSize: boardSize,
+        currentPlayer: newMatch.currentPlayer,
+        players: {black: {name: username, color: "Black"}},
+        message: "",
+        isGameActive: true,
+        hasGameStarted: false,
+        blackTime: timer,
+        whiteTime: timer
+    });
+
+    setMatch(newMatch);
+    setBoard(newMatch.board);
+    setBoardSize(boardSize);
+    setCurrentPlayer(newMatch.currentPlayer);
+    setMessage("");
+    setIsGameActive(true);
+    setHasGameStarted(false);
+    setBlackTime(timer); 
+    setWhiteTime(timer);
+};
+
+export default createNewGame;
