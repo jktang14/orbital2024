9ecc504092f1aa64c0dcee77e35367747a609e11
jktang14 <tangboy14@gmail.com>
2024-06-18 16:41:57 +0800

    Change implementation of join game

--- app/reversi/online/join-game.js
+++ app/reversi/online/join-game.js
@@ -1,28 +1,33 @@
 import { realtimeDatabase } from "@/app/firebase";
-import { ref, get, update } from "firebase/database";
+import { ref, update, onValue} from "firebase/database";
 
-export const createGame = async (gameId, playerId) => {
-    // Get the game in the database with the respective gameId
-    const gameRef = ref(realtimeDatabase, `games/${gameId}`)
-    const gameSnapshot = await get(gameRef);
+const joinGame = (inputGameId, username, setGameId) => {
+    if (inputGameId) {
+        const gameRef = ref(realtimeDatabase, `games/${inputGameId}`);
+        
+        onValue(gameRef, (snapshot) => {
+            const gameData = snapshot.val();
+            if (gameData) {
+                // Add the second player to the game
+                const updatedPlayers = {
+                    ...gameData.players,
+                    white: { name: username || 'Player 2', color: "White" }
+                };
 
-    // Check if this game exists
-    if (gameSnapshot.exists()) {
-        const gameData = gameSnapshot.val();
+                // Update game data in Firebase
+                update(gameRef, {
+                    players: updatedPlayers,
+                });
 
-        // Check if game selected has less than 2 players
-        if (Object.keys(gameData.players).length < 2) {
-            // Assign color to player joining
-            const playerColor = gameData[Object.keys(gameData.players)[0]] == "Black" ? "White" : "Black";
-            // Add player to players object
-            gameData.players[playerId] = playerColor;
-            
-            // Update players object in game
-            await update(gameRef,  {
-                players: gameData.players
-            })
-        }
+                // Set gameId in state to start listening for updates
+                setGameId(inputGameId);
+            } else {
+                alert("Game not found!");
+            }
+        });
     } else {
-        throw new error("Game not found");
+        alert("Please enter a valid Game ID!");
     }
-}
+};
+
+export default joinGame;
