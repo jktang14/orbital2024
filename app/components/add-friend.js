948213f794ed304e64c76aad8af774e354629839
jktang14 <tangboy14@gmail.com>
2024-06-22 15:33:30 +0800

    Change input to add friend

--- app/components/add-friend.js
+++ app/components/add-friend.js
@@ -1,7 +1,7 @@
 import { db } from "../firebase";
-import { query, collection, where, getDocs, getDoc, updateDoc, doc, arrayUnion} from "firebase/firestore";
+import { query, collection, where, getDocs, updateDoc, doc, arrayUnion} from "firebase/firestore";
 
-export const AddFriend = async (currentUserId, friendName) => {
+export const AddFriend = async (username, friendName) => {
     // Check if friendName exists in database
     const q = query(collection(db, 'users'), where('username', '==', friendName));
     const querySnapshot = await getDocs(q);
@@ -11,17 +11,17 @@ export const AddFriend = async (currentUserId, friendName) => {
         throw new Error('User not found');
     }
 
+    const userQuery = query(collection(db, 'users'), where('username', '==', username));
+    const userSnapshot = await getDocs(userQuery);
+
     let friendId = querySnapshot.docs[0].id
+    let currentUserId = userSnapshot.docs[0].id
 
     // Update current user friend array with new friend
     const userRef = doc(db, 'users', currentUserId);
     await updateDoc(userRef, {
         friends: arrayUnion(friendName)
     })
-
-    // Get current user username
-    const userDoc = await getDoc(userRef);
-    const username = userDoc.data().username;
 
     // Update friend's friend array
     const friendRef = doc(db, 'users', friendId);
