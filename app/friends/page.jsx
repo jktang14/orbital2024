811aa7198672b2a089d3904897e5eb2f3ef69d6f
jktang14 <tangboy14@gmail.com>
2024-06-23 23:51:31 +0800

    Update friends list with changes

--- app/friends/page.jsx
+++ app/friends/page.jsx
@@ -1,6 +1,8 @@
 'use client'
 import {React, useEffect, useState} from "react";
 import styles from './style.module.css';
+import { db } from "../firebase";
+import { query, collection, where, getDocs, updateDoc, doc, onSnapshot} from "firebase/firestore";
 import { realtimeDatabase } from "../firebase";
 import { GetFriends } from "../components/get-friends";
 
@@ -17,8 +19,30 @@ const FriendsList = () => {
             }
         }
     }, []);
+
+    // useEffect to check for updates in friends list
+    useEffect(() => {
+        const handleFriendsUpdates = async () => {
+            console.log(username)
+            if (username) {
+                const q = query(collection(db, 'users'), where('username', '==', username));
+                const querySnapshot = await getDocs(q);
     
-    //useEffect to check for updates in friends list using onSnapshot
+                const userId = querySnapshot.docs[0].id;
+                const userDoc = doc(db, 'users', userId);
+    
+                const unsubscribe = onSnapshot(userDoc, (doc) => {
+                    const userData = doc.data();
+                    setFriends(userData.friends)
+                })
+    
+                return () => unsubscribe();
+            } else {
+                console.log("Error");
+            }
+        }
+        handleFriendsUpdates();
+    }, [username])
 
     //Each friend has 
     
@@ -26,11 +50,11 @@ const FriendsList = () => {
         <div className={styles.body}>
             <div className={styles.friendsList}>
                 <h1 style = {{margin: 0}}>Friends List</h1>
-                {friends.map(friend => {
+                {friends.map(friend => (
                     <li key = {friend} className={styles.listItem}>
                         {friend}
                     </li>
-                })}
+                ))}
             </div>
         </div>
     )
