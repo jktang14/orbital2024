c188e8dc16045185a793547bde7da5f2e1e93a29
jktang14 <tangboy14@gmail.com>
2024-06-26 14:12:05 +0800

    Add react toastify for game notification

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,30 @@
 'use client'
+import {React, useState, useEffect} from 'react';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { HandlePathChange } from "./components/path-change";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { usePathname } from 'next/navigation';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const pathName = usePathname();
+  const pathsWithToast = ['/game', 'friends', 'cosmetics']
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    // Set the state to true after the component mounts
+    setMounted(true);
+  }, []);
+  
   HandlePathChange();
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      {mounted && pathsWithToast.includes(pathName) && <ToastContainer />}
     </html>
   );
 }
