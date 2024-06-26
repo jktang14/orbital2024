c607a8a93c42c20fc08d95585228be6c4a8bed62
jktang14 <tangboy14@gmail.com>
2024-06-26 17:40:38 +0800

    Add function to decline game request

---
+++ app/components/decline-game-request.js
@@ -0,0 +1,20 @@
+import { db } from "../firebase";
+import { query, collection, where, getDocs, updateDoc, doc, arrayUnion, arrayRemove} from "firebase/firestore";
+
+export const DeclineGameRequest = async (username, friendName) => {
+    // Check if friendName exists in database
+    const q = query(collection(db, 'users'), where('username', '==', friendName));
+    const querySnapshot = await getDocs(q);
+
+    // if username does not exist
+    if (querySnapshot.empty) {
+        throw new Error('User not found');
+    }
+
+    let friendId = querySnapshot.docs[0].id;
+    const friendRef = doc(db, 'users', friendId);
+
+    await updateDoc(friendRef, {
+        gameRequests: arrayRemove(username)
+    })
+}
