42b089ff2785799b397ac85be0ad7216451296d3
jktang14 <tangboy14@gmail.com>
2024-07-11 00:14:47 +0800

    Restyle cosmetics page

--- app/cosmetics/page.js
+++ app/cosmetics/page.js
@@ -4,34 +4,43 @@ import styles from './style.module.css'
 
 const CosmeticPage = () => {
     
-    const pieceMap = {'blackcurve.png': {'Black': 'blackcurve.png', 'White': 'whitecurve.png'}}
+    const pieceMap = {'black.png': {'Black': 'black.png', 'White': 'white.png'}, 'blackcurve.png': {'Black': 'blackcurve.png', 'White': 'whitecurve.png'}}
     
     const handleBoardChange = (event) => {
         const color = event.target.value;
         localStorage.setItem('boardColor', color);
     }
 
-    const handleImageClick = (event) => {
-        const url = new URL(event.target.src);
-        const link = url.pathname.split('/').pop();
-        localStorage.setItem('blackPiece', pieceMap[link]['Black']);
-        localStorage.setItem('whitePiece', pieceMap[link]['White']);
+    const handleImageClick = (path) => {
+        // const url = new URL(path);
+        // const link = url.pathname.split('/').pop();
+        localStorage.setItem('blackPiece', pieceMap[path]['Black']);
+        localStorage.setItem('whitePiece', pieceMap[path]['White']);
     }
 
     return (
         <>
             <ResponsiveAppBar/>
             <div className={styles.cosmetics}>
-                <h2>Customise your experience!</h2>
                 <div className={styles.boardColor}>
-                    <h4 style={{color: "grey", marginTop: "0", marginBottom:"10px"}}>Change your board color</h4>
+                    <h1 className={styles.heading} style = {{color: "white", marginTop: "0", marginBottom:"10px"}}>Change your board color</h1>
                     <input type="color" className={styles.colorPicker} onChange={handleBoardChange}/>
                 </div>
                 <div className={styles.pieceSelection}>
-                    <h4 style={{color: "grey", marginTop: "30px", marginBottom:"20px"}}>Change your pieces</h4>
+                    <h1 className={styles.heading} style={{color: "white", marginTop: "30px", marginBottom:"20px"}}
+                    >Change your pieces</h1>
                     <div className={styles.container}>
-                        <div>
-                            <img className={styles.item1} onClick={handleImageClick} src='blackcurve.png'/>
+                        <div className={styles.item}>
+                            <img className={styles.image} src='black.png'/>
+                            <h3 className={styles.title}>Default</h3>
+                            <p className={styles.description}>The default reversi pieces!</p>
+                            <button className={styles.selectButton} onClick={() => handleImageClick('black.png')}>Select</button>
+                        </div>
+                        <div className={styles.item}>
+                            <img className={styles.image} src='blackcurve.png'/>
+                            <h3 className={styles.title}>Crescent</h3>
+                            <p className={styles.description}>Crescent shape</p>
+                            <button className={styles.selectButton} onClick={() => handleImageClick('blackcurve.png')}>Select</button>
                         </div>
                     </div>
                 </div>
