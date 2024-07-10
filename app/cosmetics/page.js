ba660c38215a800906968d15884638f09d93b54b
jktang14 <tangboy14@gmail.com>
2024-07-10 22:19:03 +0800

    Add function to change pieces

--- app/cosmetics/page.js
+++ app/cosmetics/page.js
@@ -4,9 +4,18 @@ import styles from './style.module.css'
 
 const CosmeticPage = () => {
     
+    const pieceMap = {'blackcurve.png': {'Black': 'blackcurve.png', 'White': 'whitecurve.png'}}
+    
     const handleBoardChange = (event) => {
         const color = event.target.value;
         localStorage.setItem('boardColor', color);
+    }
+
+    const handleImageClick = (event) => {
+        const url = new URL(event.target.src);
+        const link = url.pathname.split('/').pop();
+        localStorage.setItem('blackPiece', pieceMap[link]['Black']);
+        localStorage.setItem('whitePiece', pieceMap[link]['White']);
     }
 
     return (
@@ -18,8 +27,15 @@ const CosmeticPage = () => {
                     <h4 style={{color: "grey", marginTop: "0", marginBottom:"10px"}}>Change your board color</h4>
                     <input type="color" className={styles.colorPicker} onChange={handleBoardChange}/>
                 </div>
+                <div className={styles.pieceSelection}>
+                    <h4 style={{color: "grey", marginTop: "30px", marginBottom:"20px"}}>Change your pieces</h4>
+                    <div className={styles.container}>
+                        <div>
+                            <img className={styles.item1} onClick={handleImageClick} src='blackcurve.png'/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
         </>
     )
 }
