9166a01eaece67471df828a885ef1844d82d9f0f
jktang14 <tangboy14@gmail.com>
2024-07-14 22:31:22 +0800

    Add function to check if arrays are equal

---
+++ app/components/equal-board.js
@@ -0,0 +1,13 @@
+const IsEqual = (board1, board2) => {
+    for (let i = 0; i < board1.length; i++) {
+        for (let j = 0; j < board2.length; j++) {
+            if (board1[i][j] != board2[i][j]) {
+                console.log("terminate" + i + j)
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+export default IsEqual;
