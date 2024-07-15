f1f454f4df7f6c22d84a1076cf64bd3f1ceb91b6
jktang14 <tangboy14@gmail.com>
2024-07-15 17:18:14 +0800

    Remove console log'

--- app/components/equal-board.js
+++ app/components/equal-board.js
@@ -2,7 +2,6 @@ const IsEqual = (board1, board2) => {
     for (let i = 0; i < board1.length; i++) {
         for (let j = 0; j < board2.length; j++) {
             if (board1[i][j] != board2[i][j]) {
-                console.log("terminate" + i + j)
                 return false;
             }
         }
