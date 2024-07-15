d4765cdfbd8202f94790d36288ee06db635ed75d
jktang14 <tangboy14@gmail.com>
2024-07-16 01:05:42 +0800

    Update algorithm for rating change

--- app/components/updateRating.js
+++ app/components/updateRating.js
@@ -1,7 +1,7 @@
 import { db } from "../firebase";
 import { query, collection, where, getDocs, updateDoc, doc} from "firebase/firestore";
 
-const UpdateRating = async (winnerName, loserName) => {
+const UpdateRating = async (winnerName, loserName, status="win") => {
     const winnerQuery = query(collection(db, 'users'), where('username', '==', winnerName));
     const loserQuery = query(collection(db, 'users'), where('username', '==', loserName));
     const winnerSnapshot = await getDocs(winnerQuery);
@@ -9,8 +9,20 @@ const UpdateRating = async (winnerName, loserName) => {
 
     const originalWinnerRating = winnerSnapshot.docs[0].data().rating;
     const originalLoserRating = loserSnapshot.docs[0].data().rating;
-    const newWinnerRating = originalWinnerRating + 10;
-    const newLoserRating = originalLoserRating - 10;
+    const winnerExpectedProb = 1 / (1 + Math.pow(10, (originalLoserRating - originalWinnerRating) / 400));
+    const loserExpectedProb = 1 / (1 + Math.pow(10, (originalWinnerRating - originalLoserRating) / 400));
+    let newWinnerRating;
+    let newLoserRating;
+
+    if (status == "win") {
+        newWinnerRating = originalWinnerRating + Math.floor(14 * (1 - winnerExpectedProb));
+        newLoserRating = originalLoserRating + Math.ceil(14 * (0 - loserExpectedProb));
+    } else if (status == "draw") {
+        const winCalc = 14 * (0.5 - winnerExpectedProb);
+        const loseCalc = 14 * (0.5 - loserExpectedProb)
+        newWinnerRating = originalWinnerRating + (winCalc < 0 ? Math.ceil(winCalc): Math.floor(winCalc));
+        newLoserRating = originalLoserRating + (loseCalc < 0 ? Math.ceil(loseCalc): Math.floor(loseCalc));
+    }
 
     const winnerId = winnerSnapshot.docs[0].id;
     const winnerRef = doc(db, 'users', winnerId);
